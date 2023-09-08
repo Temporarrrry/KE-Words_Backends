d2e0b4f833f35ad2040240f9eafdc5d72497f379
seate <86824703+seate@users.noreply.github.com>
2023-09-08 16:32:39 +0900

    change: dto element name change

--- src/main/java/com/example/demo/Quiz/SentenceQuiz/DTO/Request/Grade/GradeSentenceQuizTestProblemRequestDTO.java
+++ src/main/java/com/example/demo/Quiz/SentenceQuiz/DTO/Request/Grade/GradeSentenceQuizTestProblemRequestDTO.java
@@ -12,5 +12,5 @@ import java.util.List;
 public class GradeSentenceQuizTestProblemRequestDTO {
     private Long sentenceId;
 
-    private List<String> userKoreanAnswer;
+    private List<String> userAnswer;
 }
