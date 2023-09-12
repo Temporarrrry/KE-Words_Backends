5620fd432bd28e774fc12585b917a1fa92938118
seate <86824703+seate@users.noreply.github.com>
2023-09-12 16:45:05 +0900

    test: frontend test

---
+++ src/main/resources/static/icons/index.js
@@ -0,0 +1,5 @@
+export { ReactComponent as LeftArrow } from "./left.svg";
+export { ReactComponent as RightArrow } from "./right.svg";
+
+export { ReactComponent as BookmarkIcon } from "./bookmark.svg";
+export { ReactComponent as BookmarkedIcon } from "./bookmarked.svg";
